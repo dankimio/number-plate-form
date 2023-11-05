7cbaa6f85cf07d865fa9014064b13e5faadeaca9
Dan Kim <itsdanya@gmail.com>
2023-11-05 02:57:54 +0000

    switch focus

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 import './style.css'
+
+document.querySelectorAll('#form input').forEach(input => {
+  input.addEventListener('input', event => {
+    const input= event.target
+    const maxlength = input.getAttribute('maxlength')
+
+    if (input.value.length >= maxlength) {
+      event.target.nextElementSibling.focus()
+    }
+  })
+})
