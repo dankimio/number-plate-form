48de8188a3265504d0f5f9b3a379ccfee1c1b3b1
Dan Kim <itsdanya@gmail.com>
2023-11-05 02:51:03 +0000

    delete unused js

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
-// document.querySelector('#app').innerHTML = `Hello, world`
-
-setupCounter(document.querySelector('#counter'))
